510fd29d705bb01ebe48c84e7cede72ab489d81d
Txai <65023597+Txai-HvA@users.noreply.github.com>
2021-10-29 09:43:13 +0200

    Added functions and split functions up

--- server.js
+++ server.js
@@ -1,99 +1,185 @@
-const express = require('express')
+const express = require('express');
 const camelCase = require('camelcase');
-const app = express()
+const app = express();
 const path = require('path');
 const router = express.Router();
 app.use(express.static('public/css'));
  
-const fs = require('fs')
-const jsonString = fs.readFileSync('./tech-track-dataset.json')
-const dataset = JSON.parse(jsonString)
+const fs = require('fs');
+const jsonString = fs.readFileSync('./tech-track-dataset.json');
+const dataset = JSON.parse(jsonString);
 //Bron https://medium.com/@osiolabs/read-write-json-files-with-node-js-92d03cc82824
 
 // Variabellen
 let old_key;
 let new_key;
-let userCount = 0;
+let aantalGebruikers;
 let oogKleur;
 let kledingKleuren;
-let list = [];
+let newList = [];
+let aantalOvereenkomsten = 0;
+let totaalAantalKleren = 0;
+let komenKleurenOvereen = false;
+let overeenkomstenInProcenten = 0;
+let mogelijkeOogKleuren = ["blauw", "bruin", "grijs", "groen"];
+let tempOogkleuren = [];
+let nieuweOogKleurNotatie;
+let veld;
 
+// Veranderd de waarde van het gegeven antwoord op een vraag
+function changeKey(index, newValue) {
+  new_key[Object.keys(new_key)[index]] = newValue;
+  Object.defineProperty(dataset[i], new_key, {});
+}
 
-function sortList(givenList) {
-  //Checked of het gegeven antwoord komma's bevat en dus een lijst is
-  if(String(givenList).indexOf(',') >= -1) {
+//Sorteert een array op alfabetische volgorde
+function sortList(arr) {
+  //Checked of het gegeven antwoord komma's bevat en dus een array is
+  if(arr.indexOf(',') >= -1) {
     //Split antwoorden op komma
-    list = givenList.split(", ");
+    newList = arr.split(", ");
+    //Zorgt ervoor dat elk woord begint met een hoofdletter
+    newList = newList.map(item => camelCase(item, {pascalCase: true}));
+
     //Sorteert op alfabetische volgorde
-    list.sort()
-    //Geef de lijst terug en zet het om naar een String
-    return list
+    newList.sort();
+
+    return newList;
   }
 }
 
-// Veranderd de waarde van het gegeven antwoord 
-function changeKey(vraagNummer, nieuweWaarde) {
-  new_key[Object.keys(new_key)[vraagNummer]] = nieuweWaarde;
-  Object.defineProperty(dataset[i], new_key, {});
-  console.log(dataset[i]);
+//Als oogKleur het woord "donker" of "licht" bevat, haal het weg
+function verwijderDonkerLicht() {
+  if(oogKleur.indexOf('donker') >= 0 || oogKleur.indexOf('licht') >= 0) {
+    if(oogKleur.indexOf('donker') >= 0) { 
+      newList = oogKleur.split("donker");
+    } else if(oogKleur.indexOf('licht') >= 0) { 
+      newList = oogKleur.split("licht");
+    }
+    oogKleur = newList[1];
+    changeKey(1, oogKleur);
+  }
+}
+
+//Noteerd oogkleur op dezelfde manier als er 2 kleuren zijn, bijv "Blauw - Groen" inplaats van "groen blauw"
+function noteerOpDezelfdeManier() {
+  //Loop langs alle mogelijke oogkleuren
+  mogelijkeOogKleuren.forEach(mogelijkeOogKleur => {
+    //Als de gegeven oogkleur een bestaande oogkleur is voeg toe aan array
+    if(oogKleur.indexOf(mogelijkeOogKleur) >= 0) { 
+      tempOogkleuren.push(mogelijkeOogKleur);
+    }
+  });
+
+  //Als er meerdere oogkleuren zijn, zet er een streepje tussen
+  if(tempOogkleuren.length > 1) {
+    nieuweOogKleurNotatie = camelCase(tempOogkleuren[0], {pascalCase: true}) + " - " 
+        + camelCase(tempOogkleuren[1], {pascalCase: true});
+    oogKleur = nieuweOogKleurNotatie;
+    changeKey(1, oogKleur);
+  } else {
+    //Als de oogkleuren lijst leeg is en dus een oogkleur was ingevuld die niet bestaat, verander naar "Geen Antwoord"
+    nieuweOogKleurNotatie = "Geen Antwoord"
+    oogKleur = nieuweOogKleurNotatie;
+    changeKey(1, oogKleur);
+  }
+  tempOogkleuren.length = 0;
+
 }
 
 function wijzigOogkleur() {
     oogKleur = String(old_key[Object.keys(old_key)[1]]).toLowerCase();
 
+    verwijderDonkerLicht();
     //Veranderd elke eerste letter van de oogKleur naar een hoofdletter
     changeKey(1, camelCase(oogKleur, {pascalCase: true}));
-
-    //Als de oogKleur Donkerbruin is, verander het naar Bruin
-    if(oogKleur == "donkerbruin") {
-      changeKey(1, "Bruin");
-    } 
-
-    //Als de oogKleur Groen - Grijs/Blauw is, noteer het op dezelfde manier
-    if(oogKleur.indexOf('groen') >= 0) {
-      if(oogKleur.indexOf('grijs') >= 0) {
-        changeKey(1, "Groen - Grijs");
-      }
-
-      if(oogKleur.indexOf('blauw') >= 0) {
-        changeKey(1, "Groen - Blauw");
-      }
-    }
+    noteerOpDezelfdeManier();
 }
 
-function wijzigKledingKleren() {
+function wijzigKledingKleuren() {
   kledingKleuren = String(old_key[Object.keys(old_key)[8]]).toLowerCase();
   kledingKleuren = sortList(kledingKleuren);
+  changeKey(8, String(kledingKleuren));
 }
 
 //Kijkt of de oogKleur en kleuren van kleding hetzelfde zijn
 function vergelijkKleuren() {
-  if(kledingKleuren.indexOf(oogKleur) >= 0) {
-    console.log(`Oogkleur komt overeen met 1/${kledingKleuren.length} van de kleding stukken`)
+  kledingKleuren.forEach(kledingKleur => {
+    if(oogKleur.toLowerCase() == kledingKleur.toLowerCase()) {
+      aantalOvereenkomsten++;
+    }
+  });
+  totaalAantalKleren += kledingKleuren.length;
+
+  komenKleurenOvereen = String(kledingKleuren).toLowerCase().indexOf(oogKleur.toLowerCase()) >= 0;
+  if(komenKleurenOvereen) {
+    console.log(`Oogkleur komt WEL overeen met 1 van de kledingstukken.`);
   } else {
-    console.log("Oogkleur komt NIET overeen met 1 van de kleding stukken")
+    console.log("Oogkleur komt NIET overeen met 1 van de kledingstukken.");
+  }
+}
+
+//Berekend het aantal procent van overeenkomsten met oogkleur en totaal aantal kleren
+function berekenOvereenkomsten() {
+  overeenkomstenInProcenten = (aantalOvereenkomsten / totaalAantalKleren) * 100;
+  console.log(`${aantalOvereenkomsten}/${totaalAantalKleren} (${overeenkomstenInProcenten.toFixed(2)}%) van alle kleren hebben zelfde kleur als de oogkleur(en) van de gebruikers.`);  
+}
+
+//Vervangt lege velden met "Geen antwoord"
+function vervangLegeVelden() {
+  aantalVelden = Object.keys(old_key).length;
+
+  for(j = 0; j < Object.keys(old_key).length; j++) {
+    veld = old_key[Object.keys(old_key)[j]];
+    if(veld == null || veld == "") {
+      changeKey(j, "Geen antwoord");
+    }
   }
 }
 
 
-app.get('/', function (req, res) {
+let filterChars = /[@#$%^&*()_+\=\[\]{};':"\\|<>]+/;
+//Vervangt deze characters met ""
+function vervangRareChars() {
+  for(j = 0; j < Object.keys(old_key).length; j++) {
+    veld = old_key[Object.keys(old_key)[j]];
 
-  userCount = 1;
+    if(filterChars.test(veld)) {
+      console.log(veld)
+      veld = veld.replace(filterChars, '');
+      changeKey(j, veld);
+     }
+  }
+}
+//Bron https://thispointer.com/javascript-check-if-string-contains-special-characters/
+
+app.get('/', function (req, res) {
+  aantalGebruikers = 1;
+  kleurenVerbanden = 0;
   for(i = 0; i < dataset.length; i++) {
 
+    console.log(`--- Gebruiker ${aantalGebruikers} ---------------------------------------------------------`);
     old_key = dataset[i];
     new_key = old_key;
 
-
     wijzigOogkleur();
-    wijzigKledingKleren();
+    wijzigKledingKleuren();
     vergelijkKleuren();
+    vervangLegeVelden();
+    vervangRareChars();
 
-    userCount++;
+    // console.log(`Wat is je oogkleur? : ${dataset[i]["Wat is je oogkleur?"]}`);
+    // console.log(`Welke kleur kledingstukken heb je aan vandaag? : ${dataset[i][ "Welke kleur kledingstukken heb je aan vandaag? (Meerdere antwoorden mogelijk natuurlijk...)"]}`);
+    
+    console.log(dataset[i]);
+
+    aantalGebruikers++;
   }
 
-  //__dirname zorgt ervoor dat ik automatisch naar mijn project folder ga
-  res.sendFile(path.join(__dirname+'/index.html'), dataset);
+  berekenOvereenkomsten();
+
+  //__dirname zorgt ervoor dat het automatisch naar mijn project folder ga
+  res.sendFile(path.join(__dirname+'/index.html'), {});
 })
  
 app.listen(3000);
